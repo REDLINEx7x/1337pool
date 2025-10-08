b39530f3a2f595814bea7aa017942c8fed67a3f1
mohamme77 <mohamed43imo@gmail.com>
2025-10-08 05:31:07 +0100

    ok

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moamhouc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/18 20:32:14 by moamhouc          #+#    #+#             */
+/*   Updated: 2025/08/19 22:52:18 by moamhouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	src_len;
+
+	i = 0;
+	src_len = 0;
+	while (src[src_len])
+	{
+		src_len++;
+	}
+	if (size == 0)
+	{
+		return (src_len);
+	}
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (src_len);
+}
